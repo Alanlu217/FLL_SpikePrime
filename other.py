72a3edcfb30401c0ddaf6bad61a3592b7e2913ff
AlanLu <alan.lu@roboroos.org.au>
2023-05-02 21:38:53 +0930

    Create test program for moveDist and turnTo

--- other.py
+++ other.py
@@ -10,5 +10,18 @@ def _tyreClear(config):
     print("tyreClear")
 
 
+def testRun1(config):
+    d = config.drive
+
+    d.moveDist(500, heading=0)
+    d.turnTo(90)
+    d.moveDist(500, heading=90)
+    d.turnTo(180)
+    d.moveDist(500, heading=180)
+    d.turnTo(-90)
+    d.moveDist(500, heading=-90)
+    d.turnTo(0)
+
+
 def printName(config):
     config.hub.display.text(str(config.hub.system.name()), 300, 20)
